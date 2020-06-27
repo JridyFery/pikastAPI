7161375d2942dacd420f2fb4b907c023110a91ea
JridyFery <jridifer006@gmail.com>
2020-06-27 16:36:45 +0100

    Bring DB into Goland

--- models/Pokemon.go
+++ models/Pokemon.go
@@ -20,3 +20,20 @@ type Pokemon struct {
 	PokemonWidth      float64   `json:"width"`
 	Player            []*Player `gorm:"many2many:player_pokemons;"`
 }
+
+//PokemonResponse Struct
+type PokemonResponse struct {
+	PokemonName       string    `json:"pokemon_name"`
+	PokemonPrefab     string    `json:"pokemon_prefab"`
+	PokemonFBX        string    `json:"pokemon_fbx"`
+	PokemonImg        []byte    `json:"pokemon_img"`
+	PokemonisPremium  bool      `json:"pokemonis_premium"`
+	PokemonCost       int       `json:"pokemon_cost"`
+	WithDiamonds      bool      `json:"with_diamonds"`
+	PokemonPower      float64   `json:"pokemon_power"`
+	PokemonAttacktype string    `json:"pokemon_attack"`
+	PokemonATKSpeed   float64   `json:"pokemon_atkspeed"`
+	PokemonMOVSpeed   float64   `json:"pokemon_movspeed"`
+	PokemonHeight     float64   `json:"height"`
+	PokemonWidth      float64   `json:"width"`
+}
